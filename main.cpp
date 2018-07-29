360138b705a77dc0444c26640ebd918de299a6bb
merrym4n <merrym4n@gmail.com>
2018-07-30 04:05:12 +0900

    set basic

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <pcap.h>
 #include <stdio.h>
 
-void usage() {
-  printf("syntax: pcap_test <interface>\n");
-  printf("sample: pcap_test wlan0\n");
-}
+
+void usage();
+
+void print(struct pcap_pkthdr *header, const u_char *packet);
+
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -12,9 +13,10 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  char* dev = argv[1];
+  char *dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  pcap_t *handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+
   if (handle == NULL) {
     fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
     return -1;
@@ -22,13 +24,25 @@ int main(int argc, char* argv[]) {
 
   while (true) {
     struct pcap_pkthdr* header;
-    const u_char* packet;
+    const u_char *packet;
     int res = pcap_next_ex(handle, &header, &packet);
+
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
-    printf("%u bytes captured\n", header->caplen);
+
+    print(header, packet);
   }
 
   pcap_close(handle);
   return 0;
+}
+
+
+void usage() {
+  printf("syntax: pcap_test <interface>\n");
+  printf("sample: pcap_test wlan0\n");
+}
+
+void print(struct pcap_pkthdr *header, const u_char *packer) {
+  printf("%u bytes cpatured\n", header->caplen);
 }
