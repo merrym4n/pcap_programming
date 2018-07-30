bc91a7ea212434cf4b505af83b8b4512b36896f7
merrym4n <merrym4n@gmail.com>
2018-07-31 00:00:00 +0900

    minor edit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <netinet/if_ether.h>	// struct ether_header
-#include <netinet/ip.h>		// struct ip
-//#include <linux/ip.h>		// struct iphdr
+//#include <netinet/ip.h>	// struct ip
+#include <linux/ip.h>		// struct iphdr
 #include <arpa/inet.h>		// inet_ntoa
 #include <linux/tcp.h>		// tcphdr
 
@@ -10,7 +10,7 @@
 void usage();
 
 void print(struct pcap_pkthdr *header, const u_char *packet);
-void print_mac(const u_char *packet);
+int  print_mac(const u_char *packet);
 void print_ip(const u_char *packet);
 void print_port(const u_char *packet);
 void print_data(struct pcap_pkthdr *header,const u_char *packet);
@@ -53,35 +53,39 @@ void usage() {
 }
 
 void print(struct pcap_pkthdr *header, const u_char *packet) {
+	int type;
 	puts("\n======== ================");
-	printf("%5u bytes captured\n", header->caplen);
+	printf("%8u bytes captured\n", header->caplen);
 
-	print_mac(packet);
-	print_ip(packet);
-	print_port(packet);
-	print_data(header, packet);
+	type = print_mac(packet);
+	if(type == 0x0800) {
+		print_ip(packet);
+		print_port(packet);
+		print_data(header, packet);
+	}
 }
 
-void print_mac(const u_char *packet) {
-	struct ether_header *ethhdr = (struct ether_header *)packet;
+int print_mac(const u_char *packet) {
+	struct ether_header *h_eth = (struct ether_header *)packet;
 	int i;
 
-	printf("MAC\t: %02x", ethhdr->ether_shost[0]);
+	printf("MAC\t: %02x", h_eth->ether_shost[0]);
 	for(i = 1; i < 6; i++)
-		printf(":%02x", ethhdr->ether_shost[i]);
-	printf(" -> %02x", ethhdr->ether_dhost[0]);
+		printf(":%02x", h_eth->ether_shost[i]);
+	printf(" -> %02x", h_eth->ether_dhost[0]);
 	for(i = 1; i < 6; i++)
-		printf(":%02x", ethhdr->ether_dhost[i]);
+		printf(":%02x", h_eth->ether_dhost[i]);
 	printf("\n");
+	return ntohs(h_eth->ether_type);
 }
 
 void print_ip(const u_char *packet) {
-	struct ip *h_ip = (struct ip *)(packet + sizeof(struct ether_header));
-	char *s_ip = inet_ntoa(h_ip->ip_src);
-	char *d_ip = inet_ntoa(h_ip->ip_dst);
+	struct iphdr *h_ip = (struct iphdr *)(packet + sizeof(struct ether_header));
+	char *s_ip = inet_ntoa(*(struct in_addr *)&h_ip->saddr);
+	char *d_ip = inet_ntoa(*(struct in_addr *)&h_ip->daddr);
 	/* case 1 */
-	printf("IP\t: %s ->", inet_ntoa(h_ip->ip_src));
-	printf(" %s\n", inet_ntoa(h_ip->ip_dst));
+	printf("IP\t: %s ->", inet_ntoa(*(struct in_addr *)&h_ip->saddr));
+	printf(" %s\n", inet_ntoa(*(struct in_addr *)&h_ip->daddr));
 	/* case 2 */
 	//printf("IP\t: %s -> %s\n", inet_ntoa(h_ip->ip_src), inet_ntoa(h_ip->ip_dst));
 	// There is some error that i can't figure out the difference between those two things.
@@ -94,10 +98,9 @@ void print_port(const u_char *packet) {
 }
 
 void print_data(struct pcap_pkthdr *header, const u_char *packet) {
-	unsigned char *data = (unsigned char *)(packet +
-			sizeof(struct ether_header) +
-			sizeof(struct iphdr) +
-			sizeof(struct tcphdr));
+	struct iphdr *h_ip = (struct iphdr *)(packet + sizeof(struct ether_header));
+	struct tcphdr *h_tcp = (struct tcphdr *)(h_ip + h_ip->ihl*4);
+	unsigned char *data = (unsigned char *)(h_tcp + h_tcp->doff*4);
 	int i = 0;
 
 	printf("Data\t: %02x ", data[i++]);
